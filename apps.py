2c3537c8a52140a68eb00c65f8a4da199146a46b
Chris Karr <github@audacious-software.com>
2024-09-26 17:31:00 -0500

    Initial commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DockerUtilsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'docker_utils'
---
+++ management/commands/docker_update_data.py
@@ -0,0 +1,50 @@
+# pylint: disable=line-too-long
+
+# Source: https://stackoverflow.com/a/72405135
+
+import json
+import os
+
+from django.core.management.commands import loaddata
+from django.apps import apps
+
+
+def should_add_record(record):
+    arr = record['model'].split('.')
+
+    model_class = apps.get_model(app_label=arr[0], model_name=arr[1])
+
+    return not model_class.objects.filter(id=record['pk'],).exists()
+
+class Command(loaddata.Command):
+    def handle(self, *args, **options):
+        args = list(args)
+
+        for file_name in args:
+            # Read the original JSON file
+
+            with open(file_name, encoding='utf-8') as json_file:
+                json_list = json.load(json_file)
+
+            # Filter out records that already exists
+            json_list_filtered = list(filter(should_add_record, json_list))
+
+            if not json_list_filtered:
+                print(f"skip {file_name}")
+
+                continue
+
+            # Write the updated JSON file
+            file_dir_and_name, file_ext = os.path.splitext(file_name)
+
+            file_name_temp = f"{file_dir_and_name}_temp{file_ext}"
+
+            with open(file_name_temp, 'w', encoding='utf-8') as json_file_temp:
+                json.dump(json_list_filtered, json_file_temp)
+
+            # Pass the request to the actual loaddata (parent functionality)
+            # args[0] = file_name_temp
+            super().handle(file_name_temp, **options)
+
+            # You can choose to not delete the file so that you can see what was added to your records
+            os.remove(file_name_temp)
